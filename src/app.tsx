1aa349441b9ade0cfd003768f400badc447d60c5
jeanleeb <jeanleeb@outlook.com.br>
2021-09-02 12:33:20 -0300

    created Hello World app using React+TypeScript

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <header className="app-header">Hello World</header>
+    </div>
+  );
+}
+
+export default App;

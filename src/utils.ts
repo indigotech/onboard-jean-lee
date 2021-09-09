094a24c1f1d06851e6959cbca6b52cb7de0d430c
jeanleeb <jeanleeb@outlook.com.br>
2021-09-09 14:16:27 -0300

    locked react-router version and changed getAuthToken to a function

--- src/utils.ts
+++ src/utils.ts
@@ -27,4 +27,6 @@ export const validatePassword = (password: string): boolean => {
   return validPassword;
 };
 
-export const getAuthToken: string | null = localStorage.getItem('auth-token');
+export const getAuthToken = (): string | null =>{ 
+    return localStorage.getItem('auth-token')
+};
